4a0c930470a01618c160fade34cd39da6263056a
Subodh <subodhlst@gmail.com>
2025-11-28 16:07:17 +0530

    implement useMemo in archiveOptions

--- src/Test.js
+++ src/Test.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 function SlowComponent() {
   // If this is too slow on your maching, reduce the `length`
-  const words = Array.from({ length: 100_000 }, () => "WORD");
+  const words = Array.from({ length: 300_000 }, () => "WORD");
   return (
     <ul>
       {words.map((word, i) => (
